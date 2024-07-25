4901accdc81d0be6e24541c364cce55844b252bc
VeyselKaraali <veysel-karaali@outlook.de>
2024-07-25 16:10:16 +0200

    Added hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {NavigationComponent} from "./components/navigation/navigation.component";
+import {HeroSectionComponent} from "./components/hero-section/hero-section.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavigationComponent],
+  imports: [HeroSectionComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
