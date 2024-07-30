3f4ddcc64fd5efba09c14e6f2422c4e1cff0ee30
VeyselKaraali <veysel-karaali@outlook.de>
2024-07-30 13:34:53 +0200

    Defined routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {AboutMeComponent} from "./components/about-me/about-me.component";
+import {SkillsComponent} from "./components/skills/skills.component";
+import {ProjectsComponent} from "./components/projects/projects.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: '**', redirectTo: '' }
+];
