6916a586d1e7d1d1fb182d43da4946df10589d87
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-03 13:52:01 +0200

    Updated translate files and config

---
+++ src/app/consts/const.ts
@@ -0,0 +1,14 @@
+import {Language} from "../models/language";
+
+export const LANGUAGES: { [key: string]: Language } = {
+  GB_EN: {
+    code: 'gb',
+    languageShort: 'EN',
+    language: 'English'
+  },
+  DE_DE: {
+    code: 'de',
+    languageShort: 'DE',
+    language: 'Deutsch',
+  }
+};
---
+++ src/app/models/language.ts
@@ -0,0 +1,5 @@
+export interface Language {
+  code: string;
+  languageShort: string;
+  language: string;
+}
