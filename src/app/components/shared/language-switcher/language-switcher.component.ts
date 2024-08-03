498157d9d48c8b4231d53a9e0e2fff06559d5dff
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-03 13:47:55 +0200

    Implemented language-switcher component

---
+++ src/app/components/shared/language-switcher/language-switcher.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {LANGUAGES} from "../../../consts/const";
+import {Language} from "../../../models/language";
+import {TranslateService} from "@ngx-translate/core";
+
+@Component({
+  selector: 'app-language-switcher',
+  standalone: true,
+  imports: [],
+  templateUrl: './language-switcher.component.html',
+  styleUrl: './language-switcher.component.scss'
+})
+export class LanguageSwitcherComponent {
+  protected readonly LANGUAGES = LANGUAGES;
+  protected readonly Object = Object;
+  selectedLanguage: Language = LANGUAGES['GB_EN'];
+
+  constructor(public translateService: TranslateService) {
+  }
+
+  public onChangeLanguage(language: Language) {
+    this.selectedLanguage = language;
+    this.translateService.use(language.code)
+  }
+
+
+}
