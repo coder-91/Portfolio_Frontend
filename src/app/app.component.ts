1d148e6cf1e0d9c14bd709312d7eff0f006c105c
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-03 13:50:35 +0200

    Implemented theme component and service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavigationComponent} from "./components/navigation/navigation.component";
 import {HeroSectionComponent} from "./components/hero-section/hero-section.component";
+import {Theme} from "./models/theme";
+import {ThemeService} from "./services/themeService/theme.service";
+import {HeroContentComponent} from "./components/hero-section/hero-content/hero-content.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroSectionComponent, NavigationComponent],
+  imports: [HeroSectionComponent, NavigationComponent, HeroContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Veysel Karaali | Fullstack Developer';
+  currentTheme: Theme;
+
+  constructor(private themeService: ThemeService) {}
+  ngOnInit(): void {
+    this.themeService.theme$.subscribe(theme => {
+      this.currentTheme = theme;
+    })
+  }
 }
