23e57cbd140c851a8151a5f7cf0e5f3aa2793ace
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-05 01:05:19 +0200

    Updated navigation component

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,19 +1,26 @@
-import {Component, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {Theme} from "../../models/theme";
 import {TranslateModule} from "@ngx-translate/core";
 import {ThemeSwitcherComponent} from "../shared/theme-switcher/theme-switcher.component";
 import {LogoComponent} from "../shared/logo/logo.component";
 import {LanguageSwitcherComponent} from "../shared/language-switcher/language-switcher.component";
-import {BasicBtnComponent} from "../shared/basic-btn/basic-btn.component";
+import {ThemeService} from "../../services/themeService/theme.service";
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [RouterModule, TranslateModule, ThemeSwitcherComponent, LogoComponent, LanguageSwitcherComponent, BasicBtnComponent],
+  imports: [RouterModule, TranslateModule, ThemeSwitcherComponent, LogoComponent, LanguageSwitcherComponent],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
-export class NavigationComponent {
-  @Input() currentTheme: Theme
+export class NavigationComponent implements OnInit{
+  currentTheme: Theme
+
+  constructor(private themeService: ThemeService) {}
+  ngOnInit(): void {
+    this.themeService.theme$.subscribe(theme => {
+      this.currentTheme = theme;
+    })
+  }
 }
