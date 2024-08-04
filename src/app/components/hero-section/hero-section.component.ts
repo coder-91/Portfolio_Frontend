69c6317c21de200bacbbc3622e1c26636e86a889
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-05 01:06:58 +0200

    Updated hero-section component

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {NavigationComponent} from "../navigation/navigation.component";
+import {HeroContentComponent} from "./hero-content/hero-content.component";
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [],
+  imports: [
+    NavigationComponent,
+    HeroContentComponent
+  ],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
