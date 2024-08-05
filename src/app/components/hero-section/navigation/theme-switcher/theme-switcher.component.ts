659a92da5c989237b9d0f8c513ef6583f377a53f
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-05 03:34:32 +0200

    Implemented responsive theme switcher

--- src/app/components/hero-section/navigation/theme-switcher/theme-switcher.component.ts
+++ src/app/components/hero-section/navigation/theme-switcher/theme-switcher.component.ts
@@ -1,34 +1,24 @@
 import {Component, Input} from '@angular/core';
-import {MatIcon} from "@angular/material/icon";
 import {Theme} from '../../../../models/theme';
 import {ThemeService} from "../../../../services/themeService/theme.service";
 
 @Component({
   selector: 'app-theme-switcher',
   standalone: true,
-  imports: [
-    MatIcon
-  ],
+  imports: [],
   templateUrl: './theme-switcher.component.html',
   styleUrl: './theme-switcher.component.scss'
 })
 export class ThemeSwitcherComponent {
   protected readonly Theme = Theme;
-  @Input() currentTheme: Theme
-  newTheme: Theme;
-  isActive = false;
+  @Input() currentTheme: Theme;
+  isDarkTheme = false;
 
-  constructor(private themeService: ThemeService) {
-    //this.newTheme = this.themeService.getCurrentTheme();
-  }
+  constructor(private themeService: ThemeService) {}
 
   public toggleTheme() {
-    this.newTheme = this.currentTheme === Theme.DEFAULT ? Theme.DARK_THEME : Theme.DEFAULT;
-    this.themeService.setTheme(this.newTheme);
-  }
-
-  public toggleActive() {
-    this.isActive = !this.isActive;
-    this.toggleTheme();
+    this.isDarkTheme = !this.isDarkTheme;
+    const newTheme: Theme = this.currentTheme === Theme.DEFAULT ? Theme.DARK_THEME : Theme.DEFAULT;
+    this.themeService.setTheme(newTheme);
   }
 }
