db043ea1badb70f4f805a90408d4f124bc84164d
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-06 12:27:36 +0200

    Added basic button

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {BasicBtnComponent} from "../shared/basic-btn/basic-btn.component";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [
+    BasicBtnComponent
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
