a1e5cc13e702d758b4af53208b15a95f6494260b
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-14 23:33:25 +0200

    Implemented scroll to section contact

--- src/app/components/hero-section/hero-content/hero-content.component.ts
+++ src/app/components/hero-section/hero-content/hero-content.component.ts
@@ -2,12 +2,16 @@ import {Component, OnInit} from '@angular/core';
 import {Theme} from "../../../models/theme";
 import {ThemeService} from "../../../services/themeService/theme.service";
 import {BtnLinkComponent} from "../../../shared/components/buttons/btn-link/btn-link.component";
+import {ViewportScroller} from "@angular/common";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-hero-content',
   standalone: true,
   imports: [
-    BtnLinkComponent
+    BtnLinkComponent,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './hero-content.component.html',
   styleUrl: './hero-content.component.scss'
@@ -16,8 +20,7 @@ export class HeroContentComponent implements OnInit {
   protected readonly Theme = Theme;
   currentTheme: Theme;
 
-  constructor(private themeService: ThemeService) {
-  }
+  constructor(private themeService: ThemeService, private viewportScroller: ViewportScroller) {}
 
   ngOnInit(): void {
     this.themeService.currentTheme$.subscribe( currentTheme=> {
@@ -25,5 +28,7 @@ export class HeroContentComponent implements OnInit {
     })
   }
 
-
+  public scrollToSection(sectionId: string): void {
+    this.viewportScroller.scrollToAnchor(sectionId);
+  }
 }
