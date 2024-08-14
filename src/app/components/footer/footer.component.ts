8acd6019c312309ead466a427820476fb6c26582
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-14 13:29:27 +0200

    Implemented footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LogoComponent} from "../../shared/components/logo/logo.component";
+import {Theme} from "../../models/theme";
+import {ThemeService} from "../../services/themeService/theme.service";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+    imports: [
+        LogoComponent
+    ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  currentTheme: Theme;
+  currentYear: number;
 
+  constructor(private themeService: ThemeService) {
+    this.currentYear = new Date().getFullYear();
+  }
+  ngOnInit(): void {
+    this.themeService.currentTheme$.subscribe(theme => {
+      this.currentTheme = theme;
+    })
+  }
 }
