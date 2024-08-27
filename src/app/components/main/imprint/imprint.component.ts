ae0b890840fae4d69cb82d0c4d333da743139664
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-27 22:28:53 +0200

    Implemented translation of imprint

--- src/app/components/main/imprint/imprint.component.ts
+++ src/app/components/main/imprint/imprint.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {FooterComponent} from "../footer/footer.component";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-    imports: [
-        FooterComponent
-    ],
+  imports: [
+    FooterComponent,
+    TranslateModule
+  ],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
