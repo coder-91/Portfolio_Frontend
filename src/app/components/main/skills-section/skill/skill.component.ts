02f525a7aba2dc3f341dfe08ad2fc0d9bee01145
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-29 13:50:53 +0200

    Replaced img with svg tag and replaced fill with currentColor in svg files

--- src/app/components/main/skills-section/skill/skill.component.ts
+++ src/app/components/main/skills-section/skill/skill.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-skill',
@@ -7,7 +9,20 @@ import {Component, Input} from '@angular/core';
   templateUrl: './skill.component.html',
   styleUrl: './skill.component.scss'
 })
-export class SkillComponent {
+export class SkillComponent implements OnInit {
   @Input() imageName: string = '';
   @Input() skillName: string = '';
+  public svgContent: SafeHtml;
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.loadSvg();
+  }
+
+  public loadSvg() {
+    this.http.get('./assets/skills/'+ this.imageName, { responseType: 'text' })
+      .subscribe(data => {
+        this.svgContent = this.sanitizer.bypassSecurityTrustHtml(data);
+      });
+  }
 }
