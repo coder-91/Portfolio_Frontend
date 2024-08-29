b76781724c49718ec92d933be0bfbdb5891ad907
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-29 16:04:32 +0200

    Replaced img with svg tag and replaced fill with currentColor in svg files

--- src/app/components/navigation/theme-switcher/theme-switcher.component.ts
+++ src/app/components/navigation/theme-switcher/theme-switcher.component.ts
@@ -2,6 +2,9 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Theme} from '../../../models/theme';
 import {ThemeService} from "../../../services/themeService/theme.service";
 import {NgClass} from "@angular/common";
+import {SafeHtml} from "@angular/platform-browser";
+import {forkJoin, map} from "rxjs";
+import {SvgLoaderService} from "../../../services/svgLoaderService/svg-loader.service";
 
 @Component({
   selector: 'app-theme-switcher',
@@ -16,11 +19,26 @@ export class ThemeSwitcherComponent implements OnInit {
   protected readonly Theme = Theme;
   @Input() currentTheme: Theme;
   isLightTheme:boolean;
+  svgIcons: { [key: string]: SafeHtml } = {};
+  readonly iconsToLoad = {
+    light_mode: 'assets/icons/light_mode.svg',
+    dark_mode: 'assets/icons/dark_mode.svg'
+  };
 
-  constructor(private themeService: ThemeService) {}
+  constructor(private themeService: ThemeService, private svgLoaderService: SvgLoaderService) {}
 
   ngOnInit(): void {
     this.isLightTheme = !!this.currentTheme;
+
+    const svgRequests = Object.entries(this.iconsToLoad).map(([key, path]) => {
+      return this.svgLoaderService.loadSvg(path).pipe(map(svg => ({ [key]: svg })));
+    });
+
+    forkJoin(svgRequests).subscribe(results => {
+      results.forEach(result => {
+        this.svgIcons = { ...this.svgIcons, ...result };
+      });
+    });
   }
 
   public onChangeTheme() {
