e64274efe7f9fd39b6add182196972d0c4a907ce
VeyselKaraali <veysel-karaali@outlook.de>
2024-08-30 14:11:38 +0200

    Code Refactoring theme switcher

--- src/app/components/navigation/theme-switcher/theme-switcher.component.ts
+++ src/app/components/navigation/theme-switcher/theme-switcher.component.ts
@@ -4,12 +4,16 @@ import {ThemeService} from "../../../services/themeService/theme.service";
 import {NgClass} from "@angular/common";
 import {SafeHtml} from "@angular/platform-browser";
 import {SvgLoaderService} from "../../../services/svgLoaderService/svg-loader.service";
+import {SunIconComponent} from "./sun-icon/sun-icon.component";
+import {MoonIconComponent} from "./moon-icon/moon-icon.component";
 
 @Component({
   selector: 'app-theme-switcher',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    SunIconComponent,
+    MoonIconComponent
   ],
   templateUrl: './theme-switcher.component.html',
   styleUrl: './theme-switcher.component.scss'
