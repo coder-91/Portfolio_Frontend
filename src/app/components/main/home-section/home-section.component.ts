d97855e090eb744695876b0d627648ed5cbc1bf0
VeyselKaraali <veysel-karaali@outlook.de>
2024-09-14 00:59:19 +0200

    Updated styling home section

--- src/app/components/main/home-section/home-section.component.ts
+++ src/app/components/main/home-section/home-section.component.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {TranslateModule} from "@ngx-translate/core";
+import {Theme, ThemeService} from "../../../services/themeService/theme.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home-section',
@@ -10,4 +12,20 @@ import {TranslateModule} from "@ngx-translate/core";
   templateUrl: './home-section.component.html',
   styleUrl: './home-section.component.scss'
 })
-export class HomeSectionComponent {}
+export class HomeSectionComponent implements OnInit, OnDestroy {
+  protected readonly Theme = Theme;
+  currentTheme: Theme;
+  private currentThemeSubscription = new Subscription();
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.currentThemeSubscription = this.themeService.currentTheme$.subscribe( currentTheme=> {
+      this.currentTheme = currentTheme;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.currentThemeSubscription.unsubscribe()
+  }
+}
