e39fcb600b165871a7a78e5af06ef20347dd24bc
VeyselKaraali <veysel-karaali@outlook.de>
2024-09-26 13:41:15 +0200

    Updated footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,12 +3,12 @@ import {Theme, ThemeService} from "../../services/themeService/theme.service";
 import {Router, RouterLink} from "@angular/router";
 import {Subscription} from "rxjs";
 import {TranslateModule} from "@ngx-translate/core";
-import {SvgLoaderService} from "../../services/svgLoaderService/svg-loader.service";
 import {LogoV2Component} from "../shared/logos/logo-v2/logo-v2.component";
 import {GithubIconComponent} from "../shared/icons/github-icon/github-icon.component";
 import {EmailIconComponent} from "../shared/icons/email-icon/email-icon.component";
 import {LinkedinIconComponent} from "../shared/icons/linkedin-icon/linkedin-icon.component";
 import {LogoV1Component} from "../shared/logos/logo-v1/logo-v1.component";
+import {ScrollService} from "../../services/scrollService/scroll.service";
 
 @Component({
   selector: 'app-footer',
@@ -30,7 +30,7 @@ export class FooterComponent implements OnInit, OnDestroy {
   currentYear: number;
   private currentThemeSubscription = new Subscription();
 
-  constructor(private themeService: ThemeService, private router: Router, private svgLoaderService: SvgLoaderService) {
+  constructor(private themeService: ThemeService, private router: Router, private scrollService: ScrollService) {
     this.currentYear = new Date().getFullYear();
   }
   ngOnInit(): void {
@@ -43,6 +43,10 @@ export class FooterComponent implements OnInit, OnDestroy {
     this.router.navigate(['/imprint']).then(() => {
       window.scrollTo({ top: 0});
     });
+  }
+
+  public scrollToHome() {
+    this.scrollService.scrollToAnchor("home");
   }
 
   ngOnDestroy(): void {
