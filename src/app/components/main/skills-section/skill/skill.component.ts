c67f2fb28714681603cdd993dc79cdb9fae9bd96
VeyselKaraali <veysel-karaali@outlook.de>
2024-09-29 11:39:10 +0200

    Change skill images based on theme

--- src/app/components/main/skills-section/skill/skill.component.ts
+++ src/app/components/main/skills-section/skill/skill.component.ts
@@ -1,7 +1,9 @@
 import {
   Component,
-  Input,
+  Input, OnDestroy, OnInit,
 } from '@angular/core';
+import {Theme, ThemeService} from "../../../../services/themeService/theme.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-skill',
@@ -10,7 +12,22 @@ import {
   templateUrl: './skill.component.html',
   styleUrl: './skill.component.scss',
 })
-export class SkillComponent {
+export class SkillComponent implements OnInit, OnDestroy {
   @Input() skillId: string = '';
   @Input() skillName: string = '';
+  protected readonly Theme = Theme;
+  currentTheme: Theme;
+  private currentThemeSubscription = new Subscription();
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.currentThemeSubscription = this.themeService.currentTheme$.subscribe( currentTheme=> {
+      this.currentTheme = currentTheme;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.currentThemeSubscription.unsubscribe()
+  }
 }
