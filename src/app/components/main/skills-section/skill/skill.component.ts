e745f7ee8b7f890139e4377b8e1a4c4d3aa14aae
VeyselKaraali <veysel-karaali@outlook.de>
2024-09-29 11:08:39 +0200

    Updated skills section

--- src/app/components/main/skills-section/skill/skill.component.ts
+++ src/app/components/main/skills-section/skill/skill.component.ts
@@ -1,29 +1,16 @@
 import {
-  AfterViewInit,
   Component,
   Input,
 } from '@angular/core';
-import {SafeHtml} from "@angular/platform-browser";
-import {SvgLoaderService} from "../../../../services/svgLoaderService/svg-loader.service";
-import {Observable} from "rxjs";
-import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-skill',
   standalone: true,
-  imports: [
-    AsyncPipe
-  ],
+  imports: [],
   templateUrl: './skill.component.html',
   styleUrl: './skill.component.scss',
 })
-export class SkillComponent implements AfterViewInit {
-  @Input() imageName: string = '';
+export class SkillComponent {
+  @Input() skillId: string = '';
   @Input() skillName: string = '';
-  public svgContent$: Observable<SafeHtml>;
-  constructor(private svgLoaderService: SvgLoaderService) {}
-
-  ngAfterViewInit(): void {
-    this.svgContent$ = this.svgLoaderService.loadSvg(`./assets/images/skills/${this.imageName}`);
-  }
 }
