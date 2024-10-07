ff6f6a7258027aae00c78da0728f77bfaa66d477
VeyselKaraali <veysel-karaali@outlook.de>
2024-10-07 02:00:30 +0200

    Smooth scrolling to contact section

--- src/app/components/main/about-me-section/about-me-section.component.ts
+++ src/app/components/main/about-me-section/about-me-section.component.ts
@@ -2,12 +2,15 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Theme, ThemeService} from "../../../services/themeService/theme.service";
 import {Subscription} from "rxjs";
 import {TranslateModule} from "@ngx-translate/core";
+import {RouterModule} from '@angular/router';
+import {ScrollService} from "../../../services/scrollService/scroll.service";
 
 @Component({
   selector: 'app-about-me-section',
   standalone: true,
   imports: [
-    TranslateModule
+    TranslateModule,
+    RouterModule
   ],
   templateUrl: './about-me-section.component.html',
   styleUrl: './about-me-section.component.scss'
@@ -17,12 +20,16 @@ export class AboutMeSectionComponent implements OnInit, OnDestroy {
   currentTheme: Theme;
   private currentThemeSubscription = new Subscription();
 
-  constructor(private themeService: ThemeService) {}
+  constructor(private themeService: ThemeService, private scrollService:ScrollService) {}
 
   ngOnInit(): void {
     this.currentThemeSubscription = this.themeService.currentTheme$.subscribe( currentTheme=> {
       this.currentTheme = currentTheme;
     })
+  }
+
+  public scrollToAnchor(anchor: string) {
+    this.scrollService.scrollToAnchor(anchor);
   }
 
   ngOnDestroy(): void {
